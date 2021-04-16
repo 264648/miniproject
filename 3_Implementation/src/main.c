707915e7c27744e025ec48c643178632566fdb4c
264648 <81163246+264648@users.noreply.github.com>
2021-04-16 15:31:57 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,1 +1,16 @@
+#include "proto.h"
+int main()
+{
+   printf("interested playing snake game\n");
+   printf("press 1 to start game\n");
+   int n;
+   scanf("%d",&n);
+   if(n==1){
+       snakegame();
+   }
+   else{
+       printf("you entered the wrong input");
+   }
+   
+   
 
